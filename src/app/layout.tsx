8eceaec222522ab28e655b73f31c466f6bbb1b0c
Kunal <2112sainikunal@gmail.com>
2025-10-29 09:48:08 +0000

    reduce thie font thickness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Bitcount+Grid+Single:wght@400;600;700&family=Inter:wght@400;500&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Bitcount+Grid+Single:wght@400&family=Inter:wght@400;500&display=swap" rel="stylesheet" />
       </head>
       <body className={cn('font-body antialiased min-h-screen flex flex-col')}>
         <Header />
@@ -31,3 +31,5 @@ export default function RootLayout({
     </html>
   );
 }
+
+    
