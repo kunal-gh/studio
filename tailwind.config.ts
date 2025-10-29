606ee130c2be160483c490cda240eaa9a3e5a91a
Kunal <2112sainikunal@gmail.com>
2025-10-29 10:01:27 +0000

    make the edges of font little curve

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,7 +18,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Inter', 'sans-serif'],
-        headline: ['Montserrat', 'sans-serif'],
+        headline: ['Nunito', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
