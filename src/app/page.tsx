22d8123cd0b09a63888c70094d930432231d55e7
Kunal <2112sainikunal@gmail.com>
2025-10-30 07:38:34 +0000

    everthing is perfects just lock this layout jus one increase thr width o

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -96,7 +96,7 @@ const portfolioCategories = [
         title: "Concerts", 
         description: "Freezing the high-energy moments of live performances.",
         coverImage: placeHolderImages.find(p => p.id === 'concert-1')!,
-        className: "md:col-span-2",
+        className: "md:col-span-3",
     },
 ];
 
