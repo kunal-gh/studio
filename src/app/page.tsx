d504f6849ac2da021cfc9cd29a796eae7eb46837
Kunal <2112sainikunal@gmail.com>
2025-11-05 18:06:08 +0000

    nah try something else use web to search for more ideas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -314,14 +314,13 @@ export default function Home() {
                 <div className="md:col-span-1">
                     <ContactForm />
                 </div>
-                <div className="hidden md:flex h-full flex-col items-center justify-center relative">
-                    <Separator orientation="vertical" className="flex-grow" />
-                    <div className="absolute my-4 p-2 bg-card">
-                         <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" className="text-primary">
-                            <path d="M8 0L16 8L8 16L0 8L8 0Z" fill="currentColor"/>
-                         </svg>
-                    </div>
-                </div>
+                <div 
+                    className="hidden md:flex w-px h-full mx-auto"
+                    style={{
+                        backgroundImage: "radial-gradient(circle, hsl(var(--border)) 1px, transparent 1px)",
+                        backgroundSize: "1px 8px"
+                    }}
+                />
                 <div className="md:col-span-1">
                   <h3 className="font-headline text-3xl font-semibold pt-2">Contact Information</h3>
                   <div className="space-y-6 text-base mt-8">
@@ -375,3 +374,5 @@ export default function Home() {
     </>
   );
 }
+
+    
