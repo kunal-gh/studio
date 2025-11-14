5dcc45e57b806bdd73e1b67c5d835b65b491c162
kunal-gh <2112sainikunal@gmail.com>
2025-11-14 14:35:41 +0530

    Complete Firebase removal - 100% independent project

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-6358494429-d141f",
-  "appId": "1:1050184117811:web:718a6e7c6431c56bd6bd46",
-  "apiKey": "AIzaSyD__x39nsr6pDHfMwCFR9FPWFq3_ffkOF8",
-  "authDomain": "studio-6358494429-d141f.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1050184117811"
-};
